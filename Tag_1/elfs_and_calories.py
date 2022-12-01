154b71e7e9daa4e8329e54de1674a3500bcb34b3
Samuel Pernthaler <samuel.pernthaler@gmail.com>
2022-12-01 19:10:30 +0100

    Changed Structure

--- Tag_1/elfs_and_calories.py
+++ Tag_1/elfs_and_calories.py
@@ -4,10 +4,8 @@ data = my_file.read()
 data_into_list = data.split("\n")
 
 # Part 1
-
-size = len(data_into_list)
 idx_list = [idx + 1 for idx, val in enumerate(data_into_list) if val == '']
-calories =  [data_into_list[i: j] for i, j in zip([0] + idx_list, idx_list + ([size] if idx_list[-1] != size else []))]
+calories =  [data_into_list[i: j] for i, j in zip([0] + idx_list, idx_list + ([len(data_into_list)] if idx_list[-1] != len(data_into_list) else []))]
 calories_per_elf = []
 
 for i in range(0,len(calories)):
@@ -15,23 +13,12 @@ for i in range(0,len(calories)):
         calories[i].remove("")
     for x in range(0,len(calories[i])):
         calories[i][x] = int(calories[i][x])
-
     calories_per_elf.append(sum(calories[i]))
 
-d = {"elf_"+str(i): calories_per_elf[i] for i in range(1,len(calories_per_elf)) }
-
-
-
-
-sorted_elfs_by_calories = sorted(d.items(), key=lambda x:x[1])
-print(sorted_elfs_by_calories)
+sorted_elfs_by_calories = sorted(calories_per_elf)
+print("Result_1: " + str(sorted_elfs_by_calories[-1]))
 
 
 # Part 2
-x = 70374 + 68996 + 65240
-print(x)
-
-
-
-# temp_idx = test_list.index('')
-# res = [test_list[: temp_idx], test_list[temp_idx + 1: ]]
+last_3 = sorted_elfs_by_calories[-1]+sorted_elfs_by_calories[-2]+sorted_elfs_by_calories[-3]
+print("Result_2: "+ str(last_3))
